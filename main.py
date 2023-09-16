dc083e2f8856a21e103a4787c70cbccc9632a773
Sanafathima06 <144199372+Sanafathima06@users.noreply.github.com>
2023-09-16 12:11:50 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
-#bank account
+#CHALLENGE 2.2
 
-class BankAccount:
-   
-  def __init__(self,account_number,account_holder_name, initial_balance=0.0): 
-    self.__account_number=account_number
-    self.__account_holder_name=account_holder_name
-    self.__account_balance=initial_balance
-  def deposit(self,amount):
-      if amount > 0:
-        self.__account_balance += amount
-        print("Deposited ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
-      else:
-        print("Invalid deposit amount")
-  def withdraw(self,amount):
-      if amount > 0 and amount <=  self.__account_balance:
-        self.__account_balance -= amount
-        print("Withdrew ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
-      else:
-          print("Invalid withdrawal amount or insufficient balance.")
-  def display_balance(self):
-      print("Account balance for {} (Account #{}): ₹{}".format(self.__account_holder_name,
-                                                          self.__account_number, 
-                                                          self.__account_balance))
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
 
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
 
-#create an istance of the BankAccount class
-account = BankAccount(account_number="123094567",
-                      account_holder_name="Arthi.S",
-                     initial_balance=4000.0)
-
-#test deposit and withrawal fubctiinality
-
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(100.0)
-account.withdraw(1000.0)
-account.display_balance()
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+batsman=Batsman()
+bowler=Bowler()
+batsman.play()
+bowler.play()
